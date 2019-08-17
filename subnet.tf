64d11df91f034ce04c959e02effd83007da63099
Caglar Cavdar <chaglare@gmail.com>
2019-08-17 12:48:30 -0500

    subnet added

--- subnet.tf
+++ subnet.tf
@@ -81,32 +81,3 @@ resource "aws_subnet" "utility-us-west-2c-kubernetes-caglarcavdar-com" {
     "kubernetes.io/role/elb"                            = "1"
   }
 }
-
-resource "aws_vpc" "kubernetes-caglarcavdar-com" {
-  cidr_block           = "172.20.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    KubernetesCluster                                   = "kubernetes.caglarcavdar.com"
-    Name                                                = "kubernetes.caglarcavdar.com"
-    "kubernetes.io/cluster/kubernetes.caglarcavdar.com" = "owned"
-  }
-}
-
-resource "aws_vpc_dhcp_options" "kubernetes-caglarcavdar-com" {
-  domain_name         = "us-west-2.compute.internal"
-  domain_name_servers = ["AmazonProvidedDNS"]
-
-  tags = {
-    KubernetesCluster                                   = "kubernetes.caglarcavdar.com"
-    Name                                                = "kubernetes.caglarcavdar.com"
-    "kubernetes.io/cluster/kubernetes.caglarcavdar.com" = "owned"
-  }
-}
-
-resource "aws_vpc_dhcp_options_association" "kubernetes-caglarcavdar-com" {
-  vpc_id          = "${aws_vpc.kubernetes-caglarcavdar-com.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.kubernetes-caglarcavdar-com.id}"
-}
-
